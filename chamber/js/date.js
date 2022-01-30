18af00647dc87cbae465a0bbf41b67d9faff785b
belugawhale27 <ahdahlberg15@gmail.com>
2022-01-30 00:18:52 -0800

    chamber site and examples for responsive menu

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,8 +1,21 @@
+//current date and year
 const d = new Date();
 const year = d.getFullYear();
-const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-document.querySelector('#date').innerHTML = (today.toLocaleDateString("en-UK", options));
 
-
-document.querySelector('#currentyear').textContent += `${year} Ashley H. Dahlberg .:|:. San Diego, California`;
+document.querySelector('#currentyear').textContent += `${year} El Cajon Chamber of Commerce | Ashley Dahlberg | WDD230 Project`;
 document.querySelector('#lastmod').innerHTML += `Last Updated: ${document.lastModified}`;
+
+// the current date in long form
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// using a date object this is how the current date is derived
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
