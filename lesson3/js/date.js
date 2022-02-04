add8e71030db2da1f7ca4c8e5921aebd4e358f89
belugawhale27 <ahdahlberg15@gmail.com>
2022-02-04 02:58:28 -0800

    updating lesson 3

--- lesson3/js/date.js
+++ lesson3/js/date.js
@@ -1,5 +1,4 @@
 const d = new Date();
 const year = d.getFullYear();
 
-document.querySelector('#currentyear').textContent += `${year} Ashley H. Dahlberg .:|:. San Diego, California`;
-document.querySelector('#lastmod').innerHTML += `Last Updated: ${document.lastModified}`;
+document.querySelector('#date').textContent += `${year} | Ashley H. Dahlberg | Last Updated: ${document.lastModified}`;
