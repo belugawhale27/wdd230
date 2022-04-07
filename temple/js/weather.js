a7a13b6034fa06b30613ab0d554af490be6bc078
belugawhale27 <ahdahlberg15@gmail.com>
2022-04-06 22:58:58 -0700

    changes

--- temple/js/weather.js
+++ temple/js/weather.js
@@ -3,31 +3,6 @@ const sandiego = 5391811;
 const tijuana = 3981609;
 const losangeles = 5368361;
 const newportbeach = 5376890;
-// const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&units=imperial&appid=e95c965fb3bcc80c423394d563b131fb`;
-
-/* fetch(apiURL)
-    .then((response) => response.json())
-    .then((x) => {
-        console.log(x);
-
-            const currentTemp = document.querySelector('#current-temp');
-            const weathericon = document.querySelector('#weathericon');
-            const caption = document.querySelector('figcaption');
-            const cityname = document.querySelector('#city-name');
-           
-            currentTemp.textContent = x.main.temp.toFixed(0);
-            let imgsrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
-            let imgalt = x.weather[0].description;
-            imgalt = imgalt.split(' ').map(capitalize).join(' ');
-            cityname.textContent = x.name;
-
-
-            currentTemp.textContent = x.main.temp.toFixed(0);
-            weathericon.setAttribute('src', imgsrc);
-            weathericon.setAttribute('alt', imgalt);
-            caption.innerHTML = imgalt;
-          });
-*/
 
 //San Diego
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=5391811&units=imperial&appid=e95c965fb3bcc80c423394d563b131fb`;
